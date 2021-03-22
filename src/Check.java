2d950bc40bf17121cd8d01601bcb446f0365b9a2
Kjeet1 <74446319+Kjeet1@users.noreply.github.com>
2021-03-21 17:02:00 -0700

    After a new test

---
+++ src/Check.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Check {
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+int x=sc.nextInt();
+if(x==1)
+	System.out.print("Check");
+else
+	System.out.println("Bye");
+}
+}
